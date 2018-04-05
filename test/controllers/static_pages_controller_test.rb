89da87fc1b4318cac9c99817e040973ba7d37ac1
Francisco Baena <baena.francisco@gmail.com>
2018-04-05 11:43:46 +0100

    Removed watir, was causing a big timeout. Removed also dependencies

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,17 +2,6 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    # Change the local server address 
-    Rails.application.routes.default_url_options[:host]= 'localhost:3000'
-  end
-
-  test "stub watir test" do
-    browser = Watir::Browser.new
-    browser.goto static_pages_home_url
-    browser.close
-  end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
