9c8897b9af8c4f48dddba61d20c6433c7cbcf884
Francisco Baena <baena.francisco@gmail.com>
2018-04-10 19:07:29 +0100

    Implements an advanced login using persistent sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   before_save { self.email = email.downcase }
@@ -22,5 +24,37 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 6 }
   )
+
   has_secure_password
+
+  def self.digest(string)
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers an user in the database in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_diges.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets an user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
