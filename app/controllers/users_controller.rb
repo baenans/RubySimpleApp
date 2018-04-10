f509b4f40158e2f62160f47e19729401ae8f4705
Francisco Baena <baena.francisco@gmail.com>
2018-04-11 00:07:03 +0100

    Adds the server side logig for the update of users and restricts the autorization for users to edit data other than theirs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -23,6 +26,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
@@ -32,5 +45,17 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please, log in!'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
