be04481ea93d4f6b9098772eb39086bb0e202b03
Francisco Baena <baena.francisco@gmail.com>
2018-04-11 09:02:56 +0100

    style: space after colons, d'oh

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'home',  to:'static_pages#home'
-  get 'help',  to:'static_pages#help'
-  get 'about', to:'static_pages#about'
+  get 'home',  to: 'static_pages#home'
+  get 'help',  to: 'static_pages#help'
+  get 'about', to: 'static_pages#about'
 
   resources :users
   get 'users/new'
