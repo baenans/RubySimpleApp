4f4706ece46e84045e75f35fa4683c4e6639f5b8
Francisco Baena <baena.francisco@gmail.com>
2018-04-11 10:09:01 +0100

    test: tests to ensure that users can login & logout properly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,9 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper  # Include the Application Helper
+
+  # Returns true if a test user is logged in
+  def logged_in?
+    !session[:user_id].nil?
+  end
 end
