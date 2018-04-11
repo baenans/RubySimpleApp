32222103224f7c50a968ca649a9ffb7bea8ed2b8
Francisco Baena <baena.francisco@gmail.com>
2018-04-11 11:59:14 +0100

    Adds faker to automate dummy data in the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,15 @@ User.create(
   password: "12345678", 
   password_confirmation: "12345678"
 )
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@foo-bar.org"
+  password = "password"
+  User.create(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
